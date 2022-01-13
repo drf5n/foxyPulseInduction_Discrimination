3411912faeaed8245b5153ac1cdbf774e2ae5fec
David Forrest <drf5na@gmail.com>
2022-01-12 21:57:34 -0500

    Move towards Timer1 Oneshot pulse in Pin 12 per https://arduino.stackexchange.com/a/88360/6628

--- ArduinoPulseADCSample/ArduinoPulseADCSample.ino
+++ ArduinoPulseADCSample/ArduinoPulseADCSample.ino
@@ -26,6 +26,7 @@
 // Configurables:
 const byte adcPin = 0; // A0 -- Pin to read ADC data from
 const byte pulsePin = A1; // Next to A0 -- pin to pulse
+const byte oneshot_pin = 12; // OC1B pin on Mega, controlled by Timer1
 const int pulse_us = 1000; // pulse duration
 const int numSamples = 500; // sample set
 // Change the sampling speed with the prescaler config in setup()
@@ -40,11 +41,59 @@ volatile int adcReading;
 volatile unsigned long sampleEnd;
 volatile unsigned long pulseStart, pulseEnd;
 
-void setup ()
-{
-  Serial.begin (115200);
-  pinMode(pulsePin, OUTPUT);
 
+
+// ########## Timer1 One-shot functions
+// The one shot pulses are output on Digital pin OC1B (Arduino Uno D10, Mega D12, Nano D3)
+// Modified by Dave Forrest 2020-01-11 for Timer 1
+// Based on Josh Levine's Nov 23, 2015 work at https://github.com/bigjosh/TimerShot/blob/master/TimerShot.ino
+
+// Setup the one-shot pulse generator and initialize with a pulse width that is (cycles) clock counts long
+#define OSP_SET_WIDTH(cycles) (OCR1B = 0xffff-(cycles-1))
+void osp_setup(uint16_t cycles) { // 1 idles, 0xffff never matches, 2-0xfffe makes pulses
+  const byte prescaler = 0b010; // 0b001: /1,      16MHz, 62.5ns resolution, 4ms max 
+                                // 0b010: /8,       2MHz,  0.5us resolution, 32ms max
+                                // 0b011: /64,    250kHz,  4us  resolution, 263ms max
+                                // 0b100: /256,  62.5kHz  16us  resolution, 1.048s max 
+                                // 0b101: /1024, 15625Hz, 64us  resolution, 4.194176s max 
+  TCCR1B =  0;      // Halt counter by setting clock select bits to 0 (No clock source).
+  // This keeps anyhting from happening while we get set up
+  TCNT1 = 0x00;     // Start counting at bottom.
+  OCR1A = 0;      // Set TOP to 0. This effectively keeps us from counting because the counter just keeps resetting back to 0.
+  // We break out of this by manually setting the TCNT higher than 0, in which case it will count all the way up to MAX and then overflow back to 0 and get locked up again.
+  OSP_SET_WIDTH(cycles);    // This also makes new OCR values get loaded frm the buffer on every clock cycle.
+  TCCR1A = 0b11 << COM1B0  | 0b11 << WGM10; // OC1B=Set on Match, clear on BOTTOM. Mode 15 Fast PWM.
+  TCCR1B = (0b11 << WGM12) | (prescaler << CS10); // Start counting now. WGM 15=0b1111  to select Fast PWM mode
+  // Setup the OC1B pin for one-shot output:
+  //DDRB |= _BV(2);     // Uno Set pin to output (Note that OC1B = GPIO port PB2 = Arduino Uno Digital Pin 10)
+  DDRB |= _BV(6);     // Mega Set pin to output (Note that OC1B = GPIO port PB6 = Arduino Mega Digital Pin 12)
+  //DDRD |= _BV(3);     // Nano Set pin to output (Note that OC1B = GPIO port PD3 = Arduino Nano Digital Pin 3)
+}
+
+// Setup the one-shot pulse generator to idle:
+void osp_setup() {
+  osp_setup(1);  // 1 puts it in idle mode, 0xffff never triggers, 2-0xfffe produces pulses.
+}
+
+// Macro to Fire a one-shot pulse. Use the most recently set width. 
+#define OSP_FIRE() (TCNT1 = OCR1B - 1)
+
+// Macro to Test there is currently a pulse still in progress
+#define OSP_INPROGRESS() (TCNT1>0)
+
+// Macro to Fire a one-shot pulse with the specififed width.
+// Order of operations in calculating m must avoid overflow of the unint16_t.
+// TCNT2 starts one count lower than the match value because the chip will block any compare on the cycle after setting a TCNT.
+#define OSP_SET_AND_FIRE(cycles) {uint16_t m=0xffff-(cycles-1); noInterrupts();OCR1B=m;TCNT1 =m-1;interrupts();}
+
+// ##########  End of Timer1 One-shot functions
+ISR(TIMER1_OVF_vect) { //When overflow...
+  //  OCRA1=ICR1; // Constant off in WGM 14, COM1A1:0==0b11
+  TCCR1B &= ~(0b111 << CS10) ; //turn off clock
+}
+
+//######### ADC Bit-bang for free-running capture of A0
+void adc_setup_freerunning(const byte adcPin){
   // set the analog reference (high two bits of ADMUX) and select the
   // channel (low 4 bits).  this also sets ADLAR (left-adjust result)
   // to 0 (the default).
@@ -53,17 +102,25 @@ void setup ()
   // Set the ADC ADPSx prescaler flags to control sampling speed/accuracy
   ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
 
-  //ADCSRA |= bit (ADPS0);                               //   2    5 bit, 9us
-  //ADCSRA |= bit (ADPS1);                               //   4    6 bit, 9us
-  //ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8    9 bit, 9.19us
+  //ADCSRA |= bit (ADPS0);                               //   2    5 bit, 9.7us
+  //ADCSRA |= bit (ADPS1);                               //   4    6 bit, 9.7us
+  //ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8    9 bit, 9.7us
   ADCSRA |= bit (ADPS2);                                 //  16   10 bit, 13us
-  //ADCSRA |= bit (ADPS0) | bit (ADPS2);                 //  32
-  //ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64
-  //ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128
+  //ADCSRA |= bit (ADPS0) | bit (ADPS2);                 //  32     10 bit, 26us
+  //ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64     10 bit, 52us
+  //ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128     10 bit, 104us
 
   //enable automatic conversions, start them and interrupt on finish
   ADCSRA |= bit(ADATE) | bit (ADSC) | bit (ADIE);
 
+}
+
+void setup ()
+{
+  Serial.begin (115200);
+  pinMode(pulsePin, OUTPUT);
+  osp_setup(); // Config Mega Digital 12 for oneshot output
+  adc_setup_freerunning(adcPin);
 
 }  // end of setup
 
@@ -73,21 +130,26 @@ ISR (ADC_vect)
   unsigned long now_us;
   // Handle samples
   //if ( burst ) {
-    samples[sample++ ] = ADC;
-    if (sample >= numSamples)
-    {
-      sampleEnd = micros();
-      ADCSRA &= ~bit(ADIE);  // end of sampling burst so stop interrupting 
-     // burst = false;
-    }
+  samples[sample++ ] = ADC;
+  if (sample >= numSamples)
+  {
+    sampleEnd = micros();
+    ADCSRA &= ~bit(ADIE);  // end of sampling burst so stop interrupting
+    // burst = false;
+  }
   //}
-  // handle pulse end in sync with reading
+  // handle pulse in sync with reading
   // ...
+  if (sample == 1){
+    *portOutputRegister(digitalPinToPort(pulsePin)) = digitalPinToBitMask(pulsePin);
+    OSP_SET_AND_FIRE(pulse_us * 2); // 
+  }
   if (pulse && ((now_us = micros()) - pulseStart >= pulse_us )) {
     *portInputRegister(digitalPinToPort(pulsePin)) = digitalPinToBitMask(pulsePin);
     pulseEnd = now_us;
     pulse = false;
   }
+  
 
 }  // end of ADC_vect
 
@@ -142,23 +204,23 @@ void loop ()
           break;
         }
       case 'p': // start pulse
-        sample = 0; 
+        sample = 0;
         pulseStart = micros();
         //        pulseEnd = pulseStart + pulse_us;
         *portOutputRegister(digitalPinToPort(pulsePin)) |= digitalPinToBitMask(pulsePin);
         pulse = true;
-       // burst = true;
-        ADCSRA |= bit(ADIF)|bit(ADIE); // clear existing ADInterruptFlag and enable ADC Interrupts 
+        // burst = true;
+        ADCSRA |= bit(ADIF) | bit(ADIE); // clear existing ADInterruptFlag and enable ADC Interrupts
         break;
       case 'D':
       case 'd': {// report Data
           while (BURST);
           sampleDuration = sampleEnd - pulseStart;
           for (int ii = 0 ; ii < numSamples ; ii++) {
-          if (c =='D') {
-            Serial.print(ii*sampleDuration/numSamples);
-            Serial.print(' ');
-          }
+            if (c == 'D') {
+              Serial.print(ii * sampleDuration / numSamples);
+              Serial.print(' ');
+            }
             Serial.println(samples[ii]);
           }
         }
